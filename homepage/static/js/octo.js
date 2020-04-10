0274cbe850dc655855ffa2eda0f3bd1036eb04cc
gwillig <gustavwillig@gmail.com>
2020-04-11 00:40:40 +0200

    FIX| octo.css| Change color of heard font

--- homepage/static/js/octo.js
+++ homepage/static/js/octo.js
@@ -197,6 +197,25 @@ window.addEventListener("DOMContentLoaded",	() => {
           window.speechSynthesis.speak(msg);
           greeting = 2;
       }
+    if (transcript.includes("x12")) {
+            var img_tag = document.querySelector(".present")
+            img_tag.src="/static/css/herz.jpg"
+            img_tag.style.display="block"
+            var msg = new SpeechSynthesisUtterance('meow meow  meow  meow');
+                msg.volume = 1
+                msg.rate = 1.5
+                msg.pitch = 2
+                msg.lang = 'de-D'
+          window.speechSynthesis.speak(msg);
+
+          msg = new SpeechSynthesisUtterance("Herzlichen Glückwunsch zur Kette");
+                msg.volume = 1
+                msg.rate = 0.5
+                msg.pitch = 2
+                msg.lang = 'de-D'
+          window.speechSynthesis.speak(msg);
+          greeting = 2;
+      }
         //Output transcript
 
         heardOutput.textContent = `Heard: ${transcript}`;

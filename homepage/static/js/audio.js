6492c09135ded31b745f526cbe79b993c6f25dd7
gwillig <gustavwillig@gmail.com>
2020-06-14 21:30:10 +0000

    MOD|improve octo.js

--- homepage/static/js/audio.js
+++ homepage/static/js/audio.js
@@ -1,26 +1,4 @@
-var audioChunks,audioBlob,audio
-navigator.mediaDevices.getUserMedia({ audio: true})
-  .then(stream => {
-    const mediaRecorder = new MediaRecorder(stream);
-    mediaRecorder.start();
 
-    const audioChunks = [];
-    mediaRecorder.addEventListener("dataavailable", event => {
-      audioChunks.push(event.data);
-    });
-
-    mediaRecorder.addEventListener("stop", () => {
-       audioBlob = new Blob(audioChunks, { 'type' : 'audio/wav; codecs=0' });
-       audioUrl = URL.createObjectURL(audioBlob);
-       audio = new Audio(audioUrl);
-      audio.download = 'acetest.wav';
-      audio.play();console.log(audio)
-    });
-
-    setTimeout(() => {
-      mediaRecorder.stop();
-    }, 3000);
-  });
 //###########################
 //===============================================================
 function showError(msg){
